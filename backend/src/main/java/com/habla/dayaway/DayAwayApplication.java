11ecbd9ff9e3820266725587b45f16dd174b103b
npeterpal <hablaty95@gmail.com>
2025-10-14 09:03:53 +0200

    spring init

---
+++ backend/src/main/java/com/habla/dayaway/DayAwayApplication.java
@@ -0,0 +1,13 @@
+package com.habla.dayaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayAwayApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/habla/dayaway/DayAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habla.dayaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
